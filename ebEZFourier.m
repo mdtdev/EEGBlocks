52d068550056d4374b21f1bf78c4516be908a6bd
Matthew Turner <mdtdev@users.noreply.github.com>
2017-08-15 16:06:42 -0400

    Working EZFourier
    Seems to work now.

--- ebEZFourier.m
+++ ebEZFourier.m
@@ -1,21 +1,26 @@
 function [spectrum, freqAxis] = ebEZFourier(wave, Fs)
 
-% Header
+% [spectrum, freqAxis] = ebEZFourier(wave, Fs)
+%
+% Simple, one at a time, FFT of single input waves. Expects a single vector
+% as input.
 %
 % MDT
 % 2017.08.14
 % Version 0.0.1
 
+    s = size(wave);       % Make input into a  column vector
+    if s(1) < s(2)
+        wave = wave';
+    end
+
     N       = length(wave);
-    NFFT    = 2^nextpow2(N);
     window  = hamming(N);
     winwave = window .* wave;
-
-    spectrum  = fft(winwave);
-    spectrum  = sqrt(spectrum .* conj(spectrum)); 
-
-
-
-
+    
+    NFFT     = 2^nextpow2(N);
+    Y        = fft(winwave, NFFT)/N;
+    freqAxis = (Fs/2)*linspace(0,1,NFFT/2+1)';
+    spectrum = 2*abs(Y(1:NFFT/2+1));
 
 end
